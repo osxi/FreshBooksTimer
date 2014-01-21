78bc453b0d4cc0f99fa3e7b772981f0e875330ac
Jake Craige <jake@poeticsystems.com>
2014-01-20 21:13:38 -0600

    Things close to working a little bit

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,19 +1,75 @@
 'use strict';
 
+var $ = jQuery, stopwatch;
+
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
 var cardData = {};
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    //console.log(sender.tab ?
-                //"from a content script:" + sender.tab.url :
-                //"from the extension");
-    if (request.cardData) {
-      console.log(request.cardData);
-      cardData = request.cardData;
-    }
+var Timer = function(options) {
+  if(options == null) {
+    options = {};
   }
-);
+  this.hours = options.hours ? options.hours : 0.0;
+  this.notes = options.notes ? options.notes : '';
+}
+var activeTimer = new Timer();
+var openPort;
+
+chrome.runtime.onConnect.addListener(function(port) {
+  console.assert(port.name == "freshbooks-trello");
+  openPort = port;
+  port.onMessage.addListener(function(msg) {
+    if (msg.action == "startTimer") {
+      if(!activeTimer) {
+        activeTimer = new Timer();
+      }
+      if(msg.data && msg.data.notes) {
+        activeTimer.notes = msg.data.notes
+      }
+      createStopwatch(activeTimer.hours);
+      stopwatch.stopwatch('start');
+      console.log('start timer..');
+
+    } else if (msg.action == "stopTimer") {
+      stopwatch.stopwatch('stop');
+      console.log('stop timer..');
+
+    } else if (msg.action == "resetTimer") {
+      stopwatch.stopwatch('reset');
+      console.log('reset timer..');
+      activeTimer = new Timer();
+
+    }
+  });
+  port.onDisconnect.addListener(function() {
+    openPort = null;
+  });
+});
+
+var createStopwatch = function(startTime) {
+  if(stopwatch) {
+    stopwatch.stopwatch('destroy');
+  }
+  stopwatch = $('#stopwatch').stopwatch(
+    {startTime: startTime * 1000}
+  );
+  stopwatch.bind('tick.stopwatch', function(e, elapsed) {
+    var currentHours = (elapsed / 1000).toFixed(2);
+    activeTimer.hours = currentHours;
+    if(openPort) {
+      openPort.postMessage({
+        action: 'tick',
+        data: {
+          time: elapsed
+        }
+      });
+    }
+  });
+};
+
+$(function() {
+});
+
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,9 +1,34 @@
 'use strict';
-var $ = jQuery;
+var $ = jQuery, hours = 0, stopwatch;
+var port = chrome.runtime.connect({name: "freshbooks-trello"});
+
+port.onMessage.addListener(function(msg){
+  if(msg.action == 'tick' && msg.data && msg.data.time) {
+    console.log('tick..', msg.data.time);
+    stopwatch.text(Number(msg.data.time) / 1000);
+  }
+});
 
 $(function() {
+  stopwatch = $('#stopwatch');
+  $('#stopwatch').text(chrome.extension.getBackgroundPage().activeTimer.hours);
+  $('#notes').val(chrome.extension.getBackgroundPage().activeTimer.notes);
   chrome.tabs.executeScript(null, { file: "scripts/jquery.js" }, function() {
     chrome.tabs.executeScript(null, { file: "scripts/get_data.js" });
+  });
+  $('#start').click(function() {
+    port.postMessage({
+      action: 'startTimer',
+      data: {
+        notes: $('#notes').val()
+      }
+    });
+  });
+  $('#stop').click(function() {
+    port.postMessage({action: 'stopTimer'});
+  });
+  $('#reset').click(function() {
+    port.postMessage({action: 'resetTimer'});
   });
 });
 var cardData = {};
@@ -17,6 +42,9 @@ chrome.runtime.onMessage.addListener(
       }
       if(cardData.item.name) {
         $('#cardName').text(cardData.item.name);
+        if(!$('#notes').val().length > 0) {
+          $('#notes').val(cardData.item.name);
+        }
       }
       if(cardData.project.id) {
         $('#projectId').text(cardData.project.id);
